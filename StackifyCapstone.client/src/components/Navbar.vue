e6097c6abf165cf8001dbe31632cb1f743240e4b
LanceFontanilla <lancefontanilla@gmail.com>
2023-09-28 10:17:17 -0600

    navbar component

--- StackifyCapstone.client/src/components/Navbar.vue
+++ StackifyCapstone.client/src/components/Navbar.vue
@@ -1,26 +1,27 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
-    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
-      </div>
-    </router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto">
-        <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
-          </router-link>
-        </li>
-      </ul>
-      <!-- LOGIN COMPONENT HERE -->
-      <Login />
-    </div>
-  </nav>
+          <section class="row landing-navigation">
+
+          <div class="col-6 m-5 d-flex justify-content-end align-items-center">
+              <section class="row g-5">
+                <div class="col-4">
+                  <router-link :to="{ name: 'About' }" class="">
+                  <h2 class="purple">About</h2>
+                  </router-link>
+                </div>
+                <div class="col-4">
+                  <h2 class="purple"> <Login /> </h2>
+                </div>
+                <div class="col-4">
+                  <router-link :to="{ name: 'Application' }" class="">
+                  <h2 class="purple">Application</h2></router-link>
+                </div>
+              </section>
+            </div>
+        </section>
+      
+
+      
+
 </template>
 
 <script>
@@ -38,19 +39,4 @@ a:hover {
   text-decoration: none;
 }
 
-.nav-link {
-  text-transform: uppercase;
-}
-
-.navbar-nav .router-link-exact-active {
-  border-bottom: 2px solid var(--bs-success);
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
-}
-
-@media screen and (min-width: 768px) {
-  nav {
-    height: 64px;
-  }
-}
 </style>
--- StackifyCapstone.client/src/pages/AboutPage.vue
+++ StackifyCapstone.client/src/pages/AboutPage.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is the about page</h1>
+    
+    <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar.vue'
 export default {
   setup() {
     return {
 
     }
-  }
+  },
+  components: { Navbar }
 }
 </script>
+
+<style scoped lang="scss">
+
+
+
+
+</style>
