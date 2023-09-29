014e17aed5fd2e99ec9f1c6ca04674594f50cedb
Tyler Marcott <tylerjmarcott@gmail.com>
2023-09-28 19:58:58 -0600

    registered schemas

--- StackifyCapstone/src/db/DbContext.js
+++ StackifyCapstone/src/db/DbContext.js
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { CollaboratorSchema } from "../models/Collaborator.js";
+import { EventsSchema } from "../models/Event.js";
+import { TimeBlocksSchema } from "../models/TimeBlock.js";
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Collaborator = mongoose.model('Collaborator', CollaboratorSchema);
+  Event = mongoose.model('Event', EventsSchema);
+  Timeblock = mongoose.model('Timeblock', TimeBlocksSchema);
 }
 
 export const dbContext = new DbContext()
