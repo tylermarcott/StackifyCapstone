a2046fe2ddb364dc3253c825d48b641a9f61a6c9
Tyler Marcott <tylerjmarcott@gmail.com>
2023-10-01 17:00:40 -0600

    verified create event working.

---
+++ StackifyCapstone.client/src/components/CreateEventForm.vue
@@ -0,0 +1,50 @@
+<template>
+    <form @submit.prevent="createEvent">
+      <div class="mb-3">
+        <label for="eventTitle" class="form-label">Event Title</label>
+        <input v-model="eventData.title" type="title" class="form-control" id="eventTitle" placeholder="event title">
+      </div>
+      <select v-model="eventData.type" class="form-select mb-3" aria-label="Choose an event">
+        <option selected>Event Type</option>
+        <option value="Wedding">Wedding</option>
+        <option value="Birthday">Birthday</option>
+        <option value="Club">Club</option>
+        <option value="Party">Party</option>
+      </select>
+      <div class="mb-3">
+        <label for="eventDescription" class="form-label">Event Description</label>
+        <textarea v-model="eventData.description" class="form-control" id="eventDescription" rows="5"></textarea>
+      </div>
+      <button class="btn btn-dark">Create Event</button>
+    </form>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { ref } from 'vue';
+import Pop from "../utils/Pop.js";
+import {eventsService} from '../services/EventsService.js'
+import { logger } from "../utils/Logger.js";
+export default {
+  setup(){
+    const eventData = ref({})
+  return {
+    eventData,
+
+    async createEvent(){
+      try {
+        await eventsService.createEvent(eventData.value)
+      } catch (error) {
+        Pop.error(error)
+      }
+    }
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- StackifyCapstone.client/src/models/Event.js
+++ StackifyCapstone.client/src/models/Event.js
@@ -5,6 +5,7 @@ export class Event{
   constructor(data){
     this.ownerId = data.ownerId
     this.title = data.title
-    this.type = data.type
+    this.type = data.eventType
+    this.description = data.eventDescription
   }
 }
