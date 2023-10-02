1d5a746385b67e317408cce35dc5545ee937bbfa
Austin <austinjperry22@gmail.com>
2023-10-02 17:26:08 -0600

    time block list works

--- StackifyCapstone/src/services/TimeblocksService.js
+++ StackifyCapstone/src/services/TimeblocksService.js
@@ -11,15 +11,17 @@ class TimeblocksService {
         return timeblock
     }
 
-    async editTimeblock(timeblockId, updates, id) {
+    async editTimeblock(timeblockId, updates, ownerId) {
         const originalTimeblock = await dbContext.Timeblock.findById(timeblockId)
         if (!originalTimeblock) throw new Error(`Unable to find timeblock at ${timeblockId}`)
+        if (originalTimeblock.ownerId != ownerId) throw new Error("You don't own this event")
         originalTimeblock.title = updates.title != undefined ? updates.title : originalTimeblock.title
         originalTimeblock.trackList = updates.trackList != undefined ? updates.trackList : originalTimeblock.trackList
         originalTimeblock.duration = updates.duration != undefined ? updates.duration : originalTimeblock.duration
         originalTimeblock.completed = updates.completed != undefined ? updates.completed : originalTimeblock.completed
         originalTimeblock.notes = updates.notes != undefined ? updates.notes : originalTimeblock.notes
         originalTimeblock.silent = updates.silent != undefined ? updates.silent : originalTimeblock.silent
+        originalTimeblock.position = updates.position != undefined ? updates.position : originalTimeblock.position
 
         await originalTimeblock.save()
         return originalTimeblock
