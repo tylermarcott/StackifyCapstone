9a80c901a2d72a3771c43e8b26002670c36abad2
Austin <austinjperry22@gmail.com>
2023-10-02 10:59:54 -0600

    timeblock list mostly done

--- StackifyCapstone.client/src/components/TimeBlockList.vue
+++ StackifyCapstone.client/src/components/TimeBlockList.vue
@@ -1,18 +1,41 @@
 <template>
-    <div class="timeblock-list">
+    <div class="timeblock-list d-flex">
         <div v-for="timeblock in myTimeblocks" :key="timeblock.id">
         <Timeblock :timeblock="timeblock"/>
         </div>
+        <section class="w-100 align-self-end">
+            <!-- TODO add the timeblock create model here -->
+            <button class="btn btn-dark w-100 my-2">+</button>
+            <!-- TODO add the all TimeblocksModel here -->
+            <button class="btn btn-dark w-100">import timeblock</button>
+        </section>
     </div>
 </template>
 
 
 <script>
 import { AppState } from '../AppState';
-import { computed, reactive, onMounted } from 'vue';
+import { computed, reactive, onMounted, watchEffect } from 'vue';
 import Timeblock from '../components/Timeblock.vue'
+import Pop from '../utils/Pop';
+import {timeBlocksService} from '../services/TimeBlocksService'
 export default {
     setup(){
+        watchEffect(()=> {
+            AppState.account;
+            getMyTimeBlocks();
+        });
+
+        async function getMyTimeBlocks(){
+            try {
+                if(!AppState.account.id){
+                    return
+                }
+                await timeBlocksService.getMyTimeBlocks()
+            } catch (error) {
+                Pop.error(error)
+            }
+        }
     return { 
         myTimeblocks: computed(() => AppState.myTimeblocks),
      }
