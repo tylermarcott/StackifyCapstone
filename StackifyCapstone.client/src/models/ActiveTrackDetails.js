a0bc5ae1a91adb5767e82f00d2ee9bd11b42bce8
LanceFontanilla <lancefontanilla@gmail.com>
2023-10-02 10:59:06 -0600

    ActiveTrackDetails

---
+++ StackifyCapstone.client/src/models/ActiveTrackDetails.js
@@ -0,0 +1,11 @@
+export class ActiveTrackDetails {
+    constructor(data) {
+        this.id = data.item.id
+        this.name = data.item.name
+        this.artist = data.item.artists[0].name
+        this.album = data.item.album.name
+        this.progress = data.progress_ms
+        this.duration = data.item.duration_ms
+        this.picture = data.item.album.images[0].url
+    }
+}
