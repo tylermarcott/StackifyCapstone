3edf6bdca8f44cdb701e13aa88f18801da588aef
LanceFontanilla <lancefontanilla@gmail.com>
2023-10-06 18:35:31 -0600

    styling

--- StackifyCapstone.client/src/components/ActiveTimeblockMusic.vue
+++ StackifyCapstone.client/src/components/ActiveTimeblockMusic.vue
@@ -21,8 +21,8 @@
             </div>
             </div>
         <div class="col-2 text-center">
-            <button title="Unlock Playlist" v-if="timeblock.locked" @click="toggleLock()" class="btn bg-light-pink my-3"><i class="mdi mdi-lock"></i></button>
-            <button title="Lock Playlist" v-else @click="toggleLock()" class="btn bg-green my-3"><i class="mdi mdi-lock-open"></i></button>
+            <button title="Unlock Playlist" v-if="timeblock.locked" @click="toggleLock()" class="btn bg-light-pink  my-3"><i class="mdi mdi-lock"></i></button>
+            <button title="Lock Playlist" v-else @click="toggleLock()" class="bg-green my-3"><i class="mdi mdi-lock-open"></i></button>
         </div>
     </section>
 </template>
