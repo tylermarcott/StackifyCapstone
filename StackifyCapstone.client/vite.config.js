6e68992b1e4724bf370e79b93f6d2b6c7096ae34
Tyler Marcott <tylerjmarcott@gmail.com>
2023-11-17 19:34:47 -0700

    build commit

--- StackifyCapstone.client/vite.config.js
+++ StackifyCapstone.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../StackifyCapstone/wwwroot',
     sourcemap: false
   },
   server: {
